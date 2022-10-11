09b0bd91af1a0c61d782b8d474e960706a554c93
Barnabas Varga <vabarnabas@gmail.com>
2022-10-11 15:32:42 +0200

    Fix

--- services/getData.ts
+++ services/getData.ts
@@ -1,6 +1,6 @@
 export const getData = async (rosztiCode: string) => {
   const response = await fetch(
-    `https://us-central1-open-roszti.cloudfunctions.net/app/users/data/${rosztiCode}?range=2022-2023+%C5%91sz+events2022-2023+%C5%91sz+events`,
+    `https://us-central1-open-roszti.cloudfunctions.net/app/users/data/${rosztiCode}?range=2022-2023+%C5%91sz+events`,
     {
       headers: {
         Accept: "application/json",
