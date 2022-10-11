466f58a181e9405a65ad3144d0a3823c60d98746
Barnabas Varga <vabarnabas@gmail.com>
2022-10-11 15:28:56 +0200

    Test

--- services/getData.ts
+++ services/getData.ts
@@ -1,10 +1,16 @@
 export const getData = async (rosztiCode: string) => {
   const response = await fetch(
-    `https://us-central1-open-roszti.cloudfunctions.net/app/users/data/${rosztiCode}?range=2022-2023+%C5%91sz+events2022-2023+%C5%91sz+events`
-  );
+    `https://us-central1-open-roszti.cloudfunctions.net/app/users/data/${rosztiCode}?range=2022-2023+%C5%91sz+events2022-2023+%C5%91sz+events`,
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }
+  )
 
   if (response.ok) {
-    return await response.json();
+    return await response.json()
   }
-  throw new Error("Something went wrong");
-};
+  throw new Error("Something went wrong")
+}
