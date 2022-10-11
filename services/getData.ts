bc35b0408208d9f723eaa4b53786221c33399094
vabarnabas <92641199+vabarnabas@users.noreply.github.com>
2022-10-11 15:11:04 +0200

    New Semester

--- services/getData.ts
+++ services/getData.ts
@@ -1,10 +1,10 @@
 export const getData = async (rosztiCode: string) => {
   const response = await fetch(
-    `https://us-central1-open-roszti.cloudfunctions.net/app/users/data/${rosztiCode}?range=2021-2022%20tavasz%20events`
-  )
+    `https://us-central1-open-roszti.cloudfunctions.net/app/users/data/${rosztiCode}?range=2022-2023+%C5%91sz+events2022-2023+%C5%91sz+events`
+  );
 
   if (response.ok) {
-    return await response.json()
+    return await response.json();
   }
-  throw new Error("Something went wrong")
-}
+  throw new Error("Something went wrong");
+};
