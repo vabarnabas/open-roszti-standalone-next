14970f5d977aa8e8c1eac4f640d93d2e24ca1f2e
Barnabas Varga <vabarnabas@gmail.com>
2022-11-06 12:34:39 +0000

    Rollback

--- services/getData.ts
+++ services/getData.ts
@@ -1,6 +1,6 @@
 export const getData = async (rosztiCode: string) => {
   const response = await fetch(
-    `https://us-central1-open-roszti.cloudfunctions.net/app/users/data/${rosztiCode}?range=2022-2023+%C5%91sz+events`,
+    `https://us-central1-open-roszti.cloudfunctions.net/app/users/data/${rosztiCode}?range=2021-2022%20tavasz%20events`,
     {
       headers: {
         Accept: "application/json",
